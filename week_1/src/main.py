ff489f472dfab0bccb12cd60561bc72d5fdeea0d
Dimasega18 <dimasega1811@gmail.com>
2025-02-14 00:33:48 +0700

    modified data_clean, data_ingestion, and main.py

--- week_1/src/main.py
+++ week_1/src/main.py
@@ -1,49 +1,23 @@
 from src import *
 
-class etl_pipeline:
+class etl_pipeline():
+    def __init__(self, path: str):
+        if path.endswith('.csv') :
+            self.path = path
+        else :
+            raise TypeError('data must type CSV format.')
 
-    def extract(url: str, output_path: str):
-import os
-import subprocess
-import pandas as pd
+    def extract(self):
+        df = pd.read_csv(self.path)
+        return df
 
-def extract(url: str, output_path: str):
-    """
-    Mengunduh file CSV dari URL dan membacanya ke dalam Pandas DataFrame.
-    Args:
-    url (str): URL tempat file CSV diunduh.
-    output_path (str): Nama file output yang akan disimpan.
-    
-    Returns:
-    pd.DataFrame: Data dari file CSV yang diunduh.
-    
-    Raises:
-    ValueError: Jika `output_path` bukan file CSV.
-    RuntimeError: Jika `wget` gagal mengunduh file.
-    pd.errors.EmptyDataError: Jika file CSV kosong atau tidak valid.
-    Exception: Error lain saat membaca CSV.
-    """
+    def transform(self, df: pd.DataFrame):
+        df = df.dropna()        
+        df_cleaned = df.drop_duplicates()
+        return df_cleaned
 
-    if not output_path.lower().endswith('.csv') :
-        raise ValueError("Output file must be a CSV file (e.g., 'data.csv').")
-    
-    try:
-        subprocess.run(["wget", "-O", output_path, url], check=True)
-        df = pd.read_csv(output_path)
-
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Failed to download file: {e}") from e
-    
-    except pd.errors.EmptyDataError:
-        raise ValueError("The downloaded CSV file is empty or corrupt.")
-    
-    except Exception as e:
-        raise RuntimeError(f"An error occurred while reading the CSV file: {e}") from e
-    
-    return df
-
-
+    def load(self, df: pd.DataFrame, chunk_size: int, table_name: str, database_url: str):
+        connection = create_engine(database_url)
+        df.to_sql(table_name,con=connection,if_exists='append',chunksize=chunk_size, index=False)
         
-    def transform(df: pd.DataFrame):
-
-    def load():
+        
